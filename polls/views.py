d38b5187589df0d8d48142b33283e277d6ead1fc
Rohit Pai <rohitkalsankpai@gmail.com>
2024-07-10 17:23:01 +0530

    Created a basic view in polls app

--- polls/views.py
+++ polls/views.py
@@ -1,3 +1,22 @@
-from django.shortcuts import render
+'''
+    Purpose : Displays a Hello World message on the screen
+    Author : Rohit Pai
+    Date : 10th July 2024
 
-# Create your views here.
+    NOTE : 
+    This is the most basic view possible in Django. 
+    Refer to urls.py, for information on how to access/display this view
+'''
+
+# Standard Library Imports
+
+# Third Party Imports
+from django.http import HttpResponse
+
+# Local Application Imports
+
+def index(request):
+    '''
+        Displays a Hello World message on the screen
+    '''
+    return HttpResponse("Hello World. You're at the polls index.")
