983ffb3a13fd8720c18d79dd626f31998698d691
Rohit Pai <rohitkalsankpai@gmail.com>
2024-07-14 16:10:44 +0530

    1. Added __str__ methods to Question and Choice models 2. Added method to check if a Question was published recently

--- polls/models.py
+++ polls/models.py
@@ -8,13 +8,30 @@
     The fields in models represent the columns in database tables.
 '''
 
+# Standard Library Imports
+import datetime
+
+# Third Party Imports
 from django.db import models
+from django.utils import timezone
+
+# Local Application Imports
+
 
 class Question(models.Model):
     question_text = models.CharField(max_length=200)
     pub_date = models.DateTimeField("date published")
 
+    def was_published_recently(self):
+        return self.pub_date >= timezone.now() - datetime.timedelta(days=1)
+
+    def __str__(self):
+        return self.question_text
+
 class Choice(models.Model):
     question = models.ForeignKey(Question, on_delete=models.CASCADE)
     choice_text = models.CharField(max_length=200)
     votes = models.IntegerField(default=0)
+
+    def __str__(self):
+        return self.choice_text
