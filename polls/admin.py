eac4df907d597fc0dc732a9aba8bf17a5590ae13
Rohit Pai <rohitkalsankpai@gmail.com>
2024-07-14 16:20:17 +0530

    Register Question model to admin interface

--- polls/admin.py
+++ polls/admin.py
@@ -1,3 +1,15 @@
+'''
+    Purpose : Register models to have an admin interface
+    Author : Rohit Pai
+    Date : 14th July 2024
+'''
+
+# Standard Library Imports
+
+# Third Party Imports
 from django.contrib import admin
 
-# Register your models here.
+# Local Application Imports
+from .models import Question
+
+admin.site.register(Question)
