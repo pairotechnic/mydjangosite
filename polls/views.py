06aa7acaf6a068cd14dc010b8f9d59acfe3bf019
Rohit Pai <rohitkalsankpai@gmail.com>
2024-07-15 22:38:47 +0530

    1. Added detail, results and vote views 2. Updated the Purpose statement in polls/views.py docstring

--- polls/views.py
+++ polls/views.py
@@ -1,10 +1,10 @@
 '''
-    Purpose : Displays a Hello World message on the screen
+    Purpose : Collection of different views that will be displayed on the screen
     Author : Rohit Pai
     Date : 10th July 2024
 
     NOTE : 
-    This is the most basic view possible in Django. 
+    These are the most basic views possible in Django. 
     Refer to urls.py, for information on how to access/display this view
 '''
 
@@ -20,3 +20,13 @@ def index(request):
         Displays a Hello World message on the screen
     '''
     return HttpResponse("Hello World. You're at the polls index.")
+
+def detail(request, question_id):
+    return HttpResponse("You're looking at question %s" % question_id)
+
+def results(requests, question_id):
+    response = "You're looking at the results of question %s."
+    return HttpResponse(response % question_id)
+
+def vote(request_id, question_id):
+    return HttpResponse("You're voting on question %s." % question_id)
