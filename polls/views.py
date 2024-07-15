70bd82b8455e63562d3d9afb21d0579942000665
Rohit Pai <rohitkalsankpai@gmail.com>
2024-07-16 00:20:03 +0530

    Updated index view to use the template that was just created

--- polls/views.py
+++ polls/views.py
@@ -12,6 +12,7 @@
 
 # Third Party Imports
 from django.http import HttpResponse
+from django.template import loader
 
 # Local Application Imports
 from .models import Question
@@ -23,8 +24,11 @@ def index(request):
         according to publication date
     '''
     latest_question_list = Question.objects.order_by("-pub_date")[:5]
-    output = ", ".join([q.question_text for q in latest_question_list])
-    return HttpResponse(output)
+    template = loader.get_template("polls/index.html")
+    context = {
+        "latest_question_list" : latest_question_list
+    }
+    return HttpResponse(template.render(context, request))
 
 def detail(request, question_id):
     return HttpResponse("You're looking at question %s" % question_id)
