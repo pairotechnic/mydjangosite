83e958090776a72b8834a8828fe1aceb39c5c3fa
Rohit Pai <rohitkalsankpai@gmail.com>
2024-07-17 20:33:47 +0530

    Created a test to see if was_published_recently() returns False for a  Question with a future publish date

--- polls/tests.py
+++ polls/tests.py
@@ -1,3 +1,24 @@
-from django.test import TestCase
+'''
+    Purpose : Tests that ensure the application behaves as intended
+    Author : Rohit Pai
+    Date : 17th July 2024
+'''
 
-# Create your tests here.
+# Standard Library Imports
+import datetime
+
+# Third Party Imports
+from django.test import TestCase
+from django.utils import timezone
+
+# Local Application Imports
+from .models import Question
+
+class QuestionModelTests(TestCase):
+    def test_was_published_recently_with_future_question(self):
+        '''
+            was_published_recently() returns False for questions whose pub_date is in the future
+        '''
+        time = timezone.now() + datetime.timedelta(days=30)
+        future_question = Question(pub_date=time)
+        self.assertIs(future_question.was_published_recently(),False)
