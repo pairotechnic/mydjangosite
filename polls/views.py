9647713d7c227f6026410fec2e89a04ec516f04f
Rohit Pai <rohitkalsankpai@gmail.com>
2024-07-18 10:00:49 +0530

    Fixed get_quesyset method of IndexView, such that it doesn't show polls with pub_date in the future

--- polls/views.py
+++ polls/views.py
@@ -17,6 +17,7 @@ from django.shortcuts import render, get_object_or_404
 from django.db.models import F
 from django.urls import reverse
 from django.views import generic
+from django.utils import timezone
 
 # Local Application Imports
 from .models import Question, Choice
@@ -27,7 +28,8 @@ class IndexView(generic.ListView):
 
     def get_queryset(self):
         # Return the last 5 published questions 
-        return Question.objects.all().order_by("-pub_date")[:5]
+        # ( not including those set to be published in the future )
+        return Question.objects.filter(pub_date__lte=timezone.now()).order_by("-pub_date")[:5]
     
 class DetailView(generic.DetailView):
     model=Question
