278c02f8ccde5ebecdde3f67541c51e09eb5b02b
Rohit Pai <rohitkalsankpai@gmail.com>
2024-07-19 12:02:53 +0530

    Removed register() call for Choice model in admin. Instead, Choice objects are edited on the Question admin page. By default, provide enough fields for 3 choices.

--- polls/admin.py
+++ polls/admin.py
@@ -12,6 +12,10 @@ from django.contrib import admin
 # Local Application Imports
 from .models import Question, Choice
 
+class ChoiceInline(admin.StackedInline):
+    model = Choice
+    extra = 3
+
 class QuestionAdmin(admin.ModelAdmin):
     '''
         Allows us to specify the order of fields in Admin
@@ -21,6 +25,6 @@ class QuestionAdmin(admin.ModelAdmin):
         (None, {"fields" : ["question_text"]}),
         ("Date information", {"fields": ["pub_date"]}),
     ]
+    inlines = [ChoiceInline]
 
 admin.site.register(Question, QuestionAdmin)
-admin.site.register(Choice)
