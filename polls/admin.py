e5c164682749c0bf8f2a6dc02a49f9f3cf31ecee
Rohit Pai <rohitkalsankpai@gmail.com>
2024-07-19 11:53:05 +0530

    Created separate fieldsets for question text and publication date

--- polls/admin.py
+++ polls/admin.py
@@ -13,6 +13,13 @@ from django.contrib import admin
 from .models import Question
 
 class QuestionAdmin(admin.ModelAdmin):
-    fields=["pub_date", "question_text"]
+    '''
+        Allows us to specify the order of fields in Admin
+        NOTE : The first element of each tuple in fieldsets is the title of the fieldset
+    '''
+    fieldsets = [
+        (None, {"fields" : ["question_text"]}),
+        ("Date information", {"fields": ["pub_date"]}),
+    ]
 
 admin.site.register(Question, QuestionAdmin)
