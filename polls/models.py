f03bc4c37e775abc31f3f1ba49c39502aa084ed9
Rohit Pai <rohitkalsankpai@gmail.com>
2024-07-19 12:27:22 +0530

    Gave a custom column name to the pub_date field in admin page "Select question to change"

--- polls/models.py
+++ polls/models.py
@@ -14,6 +14,7 @@ import datetime
 # Third Party Imports
 from django.db import models
 from django.utils import timezone
+from django.contrib import admin
 
 # Local Application Imports
 
@@ -21,6 +22,12 @@ from django.utils import timezone
 class Question(models.Model):
     question_text = models.CharField(max_length=200)
     pub_date = models.DateTimeField("date published")
+
+    @admin.display(
+            boolean=True,
+            ordering="pub_date",
+            description="Published recently?",
+    )
 
     def was_published_recently(self):
         now = timezone.now()
